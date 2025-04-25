a675dd0c43e1786dd04f2c56e5574a1198b75476
vagheladhruv <vagheladhruv0545@gmail.com>
2025-04-25 12:29:49 +0530

    1. Count Boys and Girls in the List.py

--- LAB-6/1. Count Boys and Girls in the List.py
+++ LAB-6/1. Count Boys and Girls in the List.py
@@ -1,4 +1,8 @@
-names = [("John", "Peter"), "Sarah", "Emma", ("Mike", "Tom"), "Sophia"]
+'''1.	A list contains names of boys and girls as its elements.
+Boys’ names are stored as tuples. Write a program to find out number of boys and girls in the list. (Hint: use isinstance(ele,tuple))'''
+
+
+names = [("Dhruv", "Darsh"), "Pragati", "Priyanshi", ("Jainesh", "Aarav"), "Krupali"]
 boys_count = sum(isinstance(ele, tuple) for ele in names)
 girls_count = len(names) - boys_count
 print(f"Number of boys: {boys_count}")
