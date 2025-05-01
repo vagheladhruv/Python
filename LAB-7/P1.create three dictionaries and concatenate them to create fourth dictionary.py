8fe0bc28615bb20cae6a2354f2211cd1f93ff461
vagheladhruv <vagheladhruv0545@gmail.com>
2025-05-01 16:53:28 +0530

    dictionary1.py

--- LAB-7/P1.create three dictionaries and concatenate them to create fourth dictionary.py
+++ LAB-7/P1.create three dictionaries and concatenate them to create fourth dictionary.py
@@ -1,1 +1,10 @@
+'''
+Write a program to create three dictionaries and 
+concatenate them to create fourth dictionary.
+'''
 
+a={'a1':"DAX",'a2':"Maitri",'a3':"Dhruv"}
+b={1:"Shruti",2:"jenil",3:"Het"}
+c={1:"Patel",2:"Borad",3:"Vaghela"}
+d={**a,**b,**c}
+print("COncatenated Dictinary:",d)
